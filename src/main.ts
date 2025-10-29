08b7eda9b14e07568cc2240832e62d129b916b67
faryd <mirandafaryd@gmail.com>
2025-10-29 18:33:43 -0500

    ✨ Feat: app module added
    example app module added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  //await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Finances')
+    .setDescription('This API is about finances project')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
