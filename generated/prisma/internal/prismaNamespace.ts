7ef06220dadbfd13abff9113bc2c0bc39c4ed8d6
VarBus <marcelvarela@hotmail.com>
2025-12-03 17:53:28 -0500

    a

--- generated/prisma/internal/prismaNamespace.ts
+++ generated/prisma/internal/prismaNamespace.ts
@@ -85,8 +85,6 @@ export type PrismaVersion = {
  * Query Engine version: f09f2815f091dbba658cdcd2264306d88bb5bda6
  */
 export const prismaVersion: PrismaVersion = {
-  client: "7.0.0",
-  engine: "0c19ccc313cf9911a90d99d2ac2eb0280c76c513"
   client: "7.0.1",
   engine: "f09f2815f091dbba658cdcd2264306d88bb5bda6"
 }
